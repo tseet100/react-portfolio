0aa8c3595cd337ba2a51417340ea9dd57c6debd0
Trevor <take1seet@gmail.com>
2020-10-08 16:50:20 -0400

    added two more projects

--- src/App.js
+++ src/App.js
@@ -7,14 +7,12 @@ import Footer from './Components/Footer';
 import About from './Components/About';
 import Resume from './Components/Resume';
 import Contact from './Components/Contact';
-import Testimonials from './Components/Testimonials';
 import Portfolio from './Components/Portfolio';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      foo: 'bar',
       resumeData: {},
     };
   }
@@ -45,7 +43,6 @@ class App extends Component {
         <About data={this.state.resumeData.main} />
         <Resume data={this.state.resumeData.resume} />
         <Portfolio data={this.state.resumeData.portfolio} />
-        {/* <Testimonials data={this.state.resumeData.testimonials}/> */}
         <Contact data={this.state.resumeData.main} />
         <Footer data={this.state.resumeData.main} />
       </div>
