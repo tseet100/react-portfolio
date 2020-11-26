d9a6428af84593b76b2571787739a31e6b8c1520
Trevor <take1seet@gmail.com>
2020-11-26 12:00:32 -0500

    successful animation

--- public/js/init.js
+++ public/js/init.js
@@ -134,9 +134,29 @@ jQuery(document).ready(function ($) {
   const text = document.querySelector('.responsive-headline');
   const strText = text.textContent;
   const splitText = strText.split('');
+  console.log('hererer', splitText);
   text.textContent = '';
 
   for (let i = 0; i < splitText.length; i++) {
+    if (splitText[i] === ' ') text.innerHTML += ' ';
     text.innerHTML += '<span>' + splitText[i] + '</span>';
+  }
+
+  let char = 0;
+  let timer = setInterval(onTick, 50);
+
+  function onTick() {
+    const span = text.querySelectorAll('span')[char];
+    span.classList.add('fade');
+    char++;
+    if (char === splitText.length) {
+      complete();
+      return;
+    }
+  }
+
+  function complete() {
+    clearInterval(timer);
+    timer = null;
   }
 });
